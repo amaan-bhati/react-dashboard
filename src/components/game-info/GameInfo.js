9edd835a9cfb1b66a82b06687bfd9292f96dc1d5
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-11-23 22:52:43 +0530

    Update GameInfo.js

--- src/components/game-info/GameInfo.js
+++ src/components/game-info/GameInfo.js
@@ -14,18 +14,7 @@ const GameInfo = ({ status, winner, xIsNext }) => {
       )}
       <img src={Friends} alt="Player X and Player O" />
     </section>
-<section className="game-information">
-      {xIsNext && !winner ? (
-        <h3 className="player-x">It's your turn, player X</h3>
-      ) : !xIsNext && !winner ? (
-        <h3 className="player-o">Now you, player O! </h3>
-      ) : winner && status === 'Winner: X' ? (
-        <h3 className="player-x">Nice! I won! </h3>
-      ) : (
-        <h3 className="player-o">Wohoo! I made it!</h3>
-      )}
-      <img src={Friends} alt="Player X and Player O" />
-    </section>
+
   )
 }
 
